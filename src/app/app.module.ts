b0c593d108ac1d62bfe3cfe1c23f2a96564d0731
joaquin <rojasjoaquin129@gmail.com>
2021-09-08 19:38:04 -0300

    ejercicio2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
