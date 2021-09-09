44add20685bd97b623f0af529e907b1df66ad0c8
joaquin <rojasjoaquin129@gmail.com>
2021-09-09 18:04:09 -0300

    nose porq no anda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { ErrorComponent } from './componentes/error/error.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 const routes: Routes = [
   {path:'home',component: HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'inicio',component: HomeComponent},
   {path:'ingreso',component:LoginComponent},
-  {path:'error',component:ErrorComponent},
+  {path:'registro',component:RegistroComponent},
   {path:'quien-soy',component:QuienSoyComponent},
   {path:'',redirectTo:'inicio',pathMatch:'full'},
-  {path:'**',component:ErrorComponent},
+  {path:'contacto',component:ContactoComponent},
   
 ];
 
